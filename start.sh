8ec22dcb0ef14cf63e73b608e82cf3200737c45d
Nasir Gaayte <slowboyz12@gmail.com>
2026-02-12 14:33:17 +0300

    commit from mac6

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 set -e
 
 echo "Applying database migrations..."
-python manage.py migrate
+python3 manage.py migrate
 
 echo "Collecting static files..."
-python manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 echo "Starting server..."
-python manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
